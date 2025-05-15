8052ea3a2e70806a4c868aa58815f7955f970d34
vic-7oria <victoriagracedmingo@gmail.com>
2025-05-15 14:51:19 +0800

    Add project files.

---
+++ PAGE_REP_ALGO_SIMULATION/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        cmbFrame = New ComboBox()
+        btnShow = New Button()
+        lblPgRepStr = New Label()
+        pgRefStr = New Label()
+        Label1 = New Label()
+        Label2 = New Label()
+        btnGenerate = New Button()
+        ppnlOutput = New Panel()
+        lblFrames = New Label()
+        Label5 = New Label()
+        lblRefString = New Label()
+        Label4 = New Label()
+        pnlOutput = New Panel()
+        lblPageFault = New Label()
+        Label3 = New Label()
+        cmbAlgo = New ComboBox()
+        lblPageRef = New Label()
+        ppnlOutput.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' cmbFrame
+        ' 
+        cmbFrame.FormattingEnabled = True
+        cmbFrame.Location = New Point(415, 83)
+        cmbFrame.Margin = New Padding(4)
+        cmbFrame.Name = "cmbFrame"
+        cmbFrame.Size = New Size(180, 33)
+        cmbFrame.TabIndex = 0
+        ' 
+        ' btnShow
+        ' 
+        btnShow.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        btnShow.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnShow.ForeColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        btnShow.Location = New Point(931, 111)
+        btnShow.Margin = New Padding(4)
+        btnShow.Name = "btnShow"
+        btnShow.Size = New Size(160, 52)
+        btnShow.TabIndex = 2
+        btnShow.Text = "Show"
+        btnShow.UseVisualStyleBackColor = True
+        ' 
+        ' lblPgRepStr
+        ' 
+        lblPgRepStr.AutoSize = True
+        lblPgRepStr.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold)
+        lblPgRepStr.ForeColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        lblPgRepStr.Location = New Point(56, 31)
+        lblPgRepStr.Margin = New Padding(4, 0, 4, 0)
+        lblPgRepStr.Name = "lblPgRepStr"
+        lblPgRepStr.Size = New Size(342, 38)
+        lblPgRepStr.TabIndex = 4
+        lblPgRepStr.Text = "Page Reference String: "
+        ' 
+        ' pgRefStr
+        ' 
+        pgRefStr.AutoSize = True
+        pgRefStr.Font = New Font("Segoe UI", 12F)
+        pgRefStr.Location = New Point(300, 31)
+        pgRefStr.Margin = New Padding(4, 0, 4, 0)
+        pgRefStr.Name = "pgRefStr"
+        pgRefStr.Size = New Size(0, 32)
+        pgRefStr.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold)
+        Label1.ForeColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        Label1.Location = New Point(28, 83)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(370, 38)
+        Label1.TabIndex = 6
+        Label1.Text = "Number of Page Frames: "
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Right
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold)
+        Label2.ForeColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        Label2.Location = New Point(813, 218)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(172, 38)
+        Label2.TabIndex = 7
+        Label2.Text = "Page Fault:"
+        ' 
+        ' btnGenerate
+        ' 
+        btnGenerate.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        btnGenerate.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnGenerate.ForeColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        btnGenerate.Location = New Point(844, 31)
+        btnGenerate.Margin = New Padding(4)
+        btnGenerate.Name = "btnGenerate"
+        btnGenerate.Size = New Size(247, 52)
+        btnGenerate.TabIndex = 8
+        btnGenerate.Text = "Generate String"
+        btnGenerate.UseVisualStyleBackColor = True
+        ' 
+        ' ppnlOutput
+        ' 
+        ppnlOutput.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        ppnlOutput.BackColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        ppnlOutput.Controls.Add(lblFrames)
+        ppnlOutput.Controls.Add(Label5)
+        ppnlOutput.Controls.Add(lblRefString)
+        ppnlOutput.Controls.Add(Label4)
+        ppnlOutput.Controls.Add(pnlOutput)
+        ppnlOutput.Location = New Point(12, 274)
+        ppnlOutput.Name = "ppnlOutput"
+        ppnlOutput.Size = New Size(1095, 393)
+        ppnlOutput.TabIndex = 14
+        ' 
+        ' lblFrames
+        ' 
+        lblFrames.AutoSize = True
+        lblFrames.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblFrames.ForeColor = Color.FromArgb(CByte(5), CByte(27), CByte(30))
+        lblFrames.Location = New Point(292, 47)
+        lblFrames.Margin = New Padding(4, 0, 4, 0)
+        lblFrames.Name = "lblFrames"
+        lblFrames.Size = New Size(39, 38)
+        lblFrames.TabIndex = 24
+        lblFrames.Text = "--"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.ForeColor = Color.FromArgb(CByte(5), CByte(27), CByte(30))
+        Label5.Location = New Point(156, 47)
+        Label5.Margin = New Padding(4, 0, 4, 0)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(126, 38)
+        Label5.TabIndex = 23
+        Label5.Text = "Frames:"
+        ' 
+        ' lblRefString
+        ' 
+        lblRefString.AutoSize = True
+        lblRefString.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblRefString.ForeColor = Color.FromArgb(CByte(5), CByte(27), CByte(30))
+        lblRefString.Location = New Point(292, 9)
+        lblRefString.Margin = New Padding(4, 0, 4, 0)
+        lblRefString.Name = "lblRefString"
+        lblRefString.Size = New Size(39, 38)
+        lblRefString.TabIndex = 22
+        lblRefString.Text = "--"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = Color.FromArgb(CByte(5), CByte(27), CByte(30))
+        Label4.Location = New Point(26, 9)
+        Label4.Margin = New Padding(4, 0, 4, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(258, 38)
+        Label4.TabIndex = 21
+        Label4.Text = "Reference String:"
+        ' 
+        ' pnlOutput
+        ' 
+        pnlOutput.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        pnlOutput.BackColor = Color.FromArgb(CByte(5), CByte(27), CByte(30))
+        pnlOutput.BorderStyle = BorderStyle.FixedSingle
+        pnlOutput.Location = New Point(26, 99)
+        pnlOutput.Name = "pnlOutput"
+        pnlOutput.Size = New Size(1041, 275)
+        pnlOutput.TabIndex = 0
+        ' 
+        ' lblPageFault
+        ' 
+        lblPageFault.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Right
+        lblPageFault.AutoSize = True
+        lblPageFault.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold)
+        lblPageFault.ForeColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        lblPageFault.Location = New Point(993, 218)
+        lblPageFault.Margin = New Padding(4, 0, 4, 0)
+        lblPageFault.Name = "lblPageFault"
+        lblPageFault.Size = New Size(39, 38)
+        lblPageFault.TabIndex = 15
+        lblPageFault.Text = "--"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold)
+        Label3.ForeColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        Label3.Location = New Point(220, 130)
+        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(169, 38)
+        Label3.TabIndex = 19
+        Label3.Text = "Algorithm:"
+        ' 
+        ' cmbAlgo
+        ' 
+        cmbAlgo.FormattingEnabled = True
+        cmbAlgo.Location = New Point(415, 130)
+        cmbAlgo.Margin = New Padding(4)
+        cmbAlgo.Name = "cmbAlgo"
+        cmbAlgo.Size = New Size(180, 33)
+        cmbAlgo.TabIndex = 20
+        ' 
+        ' lblPageRef
+        ' 
+        lblPageRef.AutoSize = True
+        lblPageRef.Font = New Font("Segoe UI Black", 14F, FontStyle.Bold)
+        lblPageRef.ForeColor = Color.FromArgb(CByte(206), CByte(171), CByte(205))
+        lblPageRef.Location = New Point(406, 31)
+        lblPageRef.Margin = New Padding(4, 0, 4, 0)
+        lblPageRef.Name = "lblPageRef"
+        lblPageRef.Size = New Size(39, 38)
+        lblPageRef.TabIndex = 21
+        lblPageRef.Text = "--"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(144F, 144F)
+        AutoScaleMode = AutoScaleMode.Dpi
+        AutoScroll = True
+        BackColor = Color.FromArgb(CByte(5), CByte(27), CByte(30))
+        ClientSize = New Size(1141, 689)
+        Controls.Add(lblPageRef)
+        Controls.Add(cmbAlgo)
+        Controls.Add(Label3)
+        Controls.Add(lblPageFault)
+        Controls.Add(ppnlOutput)
+        Controls.Add(btnGenerate)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(pgRefStr)
+        Controls.Add(lblPgRepStr)
+        Controls.Add(btnShow)
+        Controls.Add(cmbFrame)
+        Margin = New Padding(4)
+        MinimumSize = New Size(596, 589)
+        Name = "Form1"
+        Text = " "
+        ppnlOutput.ResumeLayout(False)
+        ppnlOutput.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents cmbFrame As ComboBox
+    Friend WithEvents VScrollBar1 As VScrollBar
+    Friend WithEvents btnShow As Button
+    Friend WithEvents lblPgRepStr As Label
+    Friend WithEvents pgRefStr As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents ppnlOutput As Panel
+    Friend WithEvents pnlOutput As Panel
+    Friend WithEvents lblPageFault As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cmbAlgo As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblFrames As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblRefString As Label
+    Friend WithEvents lblPageRef As Label
+
+End Class
---
+++ PAGE_REP_ALGO_SIMULATION/Form1.vb
@@ -0,0 +1,233 @@
+﻿Public Class Form1
+
+    Private pageReferences As New List(Of Integer)  ' To store page references
+    Private random As New Random()
+    Private Frames As Integer = 1
+    Private pageFaults As Integer = 0
+
+
+    Private Sub FIFO_Algorithm(ByRef frameList As List(Of Integer),
+                          ByRef frameSet As HashSet(Of Integer),
+                          ByRef pointer As Integer,
+                          pageNum As Integer,
+                          ByRef isPageFault As Boolean)
+        If Not frameSet.Contains(pageNum) Then
+            isPageFault = True
+            pageFaults += 1
+
+            If frameList.Count < Frames Then
+                frameList.Add(pageNum)
+                frameSet.Add(pageNum)
+            Else
+                Dim removedPage As Integer = frameList(pointer)
+                frameSet.Remove(removedPage)
+                frameList(pointer) = pageNum
+                frameSet.Add(pageNum)
+                pointer = (pointer + 1) Mod Frames
+            End If
+        End If
+    End Sub
+
+    Private Sub LRU_Algorithm(ByRef frameList As List(Of Integer),
+                         ByRef frameSet As HashSet(Of Integer),
+                         pageNum As Integer,
+                         ByRef isPageFault As Boolean)
+        If frameSet.Contains(pageNum) Then
+            frameList.Remove(pageNum)
+            frameList.Add(pageNum)
+        Else
+            isPageFault = True
+            pageFaults += 1
+
+            If frameList.Count < Frames Then
+                frameList.Add(pageNum)
+                frameSet.Add(pageNum)
+            Else
+                Dim removedPage As Integer = frameList(0)
+                frameSet.Remove(removedPage)
+                frameList.RemoveAt(0)
+                frameList.Add(pageNum)
+                frameSet.Add(pageNum)
+            End If
+        End If
+    End Sub
+
+    Private Sub OPT_Algorithm(ByRef frameList As List(Of Integer),
+                         ByRef frameSet As HashSet(Of Integer),
+                         pageNum As Integer,
+                         refIndex As Integer,
+                         ByRef isPageFault As Boolean)
+        If Not frameSet.Contains(pageNum) Then
+            isPageFault = True
+            pageFaults += 1
+
+            If frameList.Count < Frames Then
+                frameList.Add(pageNum)
+                frameSet.Add(pageNum)
+            Else
+                Dim farthestIndex As Integer = -1
+                Dim replaceIndex As Integer = 0
+
+                For i As Integer = 0 To frameList.Count - 1
+                    Dim nextUse As Integer = pageReferences.IndexOf(frameList(i), refIndex + 1)
+                    If nextUse = -1 Then
+                        replaceIndex = i
+                        Exit For
+                    ElseIf nextUse > farthestIndex Then
+                        farthestIndex = nextUse
+                        replaceIndex = i
+                    End If
+                Next
+
+                frameSet.Remove(frameList(replaceIndex))
+                frameList(replaceIndex) = pageNum
+                frameSet.Add(pageNum)
+            End If
+        End If
+    End Sub
+
+
+    Private Sub DisplayAlgorithms()
+        pnlOutput.Controls.Clear()
+        pageFaults = 0
+
+        ' Initialize data structures
+        Dim frameList As New List(Of Integer)()
+        Dim frameSet As New HashSet(Of Integer)()
+        Dim pointer As Integer = 0 ' Only used for FIFO
+        Dim xOffset As Integer = 0
+
+        For refIndex As Integer = 0 To pageReferences.Count - 1
+            Dim pageNum As Integer = pageReferences(refIndex)
+            Dim isPageFault As Boolean = False
+
+            ' Create visualization panel
+            Dim stepPanel As New Panel()
+            stepPanel.Width = 80
+            stepPanel.Height = 490
+            stepPanel.Left = xOffset
+            stepPanel.Top = 20
+            stepPanel.BackColor = Color.FromArgb(5, 27, 30)
+            stepPanel.BorderStyle = BorderStyle.None
+
+            ' Execute selected algorithm
+            Select Case cmbAlgo.SelectedItem.ToString()
+                Case "FIFO"
+                    FIFO_Algorithm(frameList, frameSet, pointer, pageNum, isPageFault)
+                Case "LRU"
+                    LRU_Algorithm(frameList, frameSet, pageNum, isPageFault)
+                Case "OPTIMAL"
+                    OPT_Algorithm(frameList, frameSet, pageNum, refIndex, isPageFault)
+            End Select
+
+            ' Draw frames visualization
+            Dim yOffset As Integer = 10
+            For i As Integer = 0 To Frames - 1
+                Dim lbl As New Label()
+                lbl.Text = If(i < frameList.Count, frameList(i).ToString(), " ")
+                lbl.Width = 80
+                lbl.Height = 50
+                lbl.Left = 20
+                lbl.Top = yOffset
+                lbl.TextAlign = ContentAlignment.MiddleCenter
+                lbl.BackColor = If(i < frameList.Count, Color.FromArgb(206, 171, 205), Color.WhiteSmoke)
+                lbl.BorderStyle = BorderStyle.FixedSingle
+                lbl.Padding = New Padding(5)
+                stepPanel.Controls.Add(lbl)
+                yOffset += 40
+            Next
+
+            ' Add fault indicator
+            Dim faultLabel As New Label()
+            faultLabel.Width = 40
+            faultLabel.Height = 30
+            faultLabel.Left = 20
+            faultLabel.Top = stepPanel.Height - 50
+            faultLabel.TextAlign = ContentAlignment.MiddleCenter
+            faultLabel.Text = If(isPageFault, "!", ".")
+            faultLabel.Font = New Font(faultLabel.Font, FontStyle.Bold)
+            faultLabel.ForeColor = If(isPageFault, Color.FromArgb(206, 171, 205), Color.FromArgb(129, 24, 68))
+            stepPanel.Controls.Add(faultLabel)
+
+            ' Add reference label
+            Dim refLabel As New Label()
+            refLabel.Text = pageNum.ToString()
+            refLabel.Width = 60
+            refLabel.Height = 30
+            refLabel.Left = 20
+            refLabel.Top = 5
+            refLabel.TextAlign = ContentAlignment.MiddleCenter
+            refLabel.Font = New Font(refLabel.Font, FontStyle.Bold)
+            stepPanel.Controls.Add(refLabel)
+
+            pnlOutput.Controls.Add(stepPanel)
+            xOffset += 120
+        Next
+
+        ' Final UI updates
+        lblPageFault.Text = pageFaults.ToString()
+        pnlOutput.HorizontalScroll.Enabled = (xOffset > pnlOutput.Width)
+        pnlOutput.AutoScroll = True
+    End Sub
+
+
+    Private Sub btnGen_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        ' Clear previous page references
+        pageReferences.Clear()
+
+
+        ' Generate random page references (between 20 and 25)
+        Dim numPages As Integer = random.Next(20, 25)
+        For i As Integer = 1 To numPages
+            pageReferences.Add(random.Next(0, 10)) ' Store randomly generated numbers, between 0 to 9, in pageReferences
+        Next
+
+        ' Display reference string in the Label
+        lblPageRef.Text = String.Join(" ", pageReferences)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' lists the no. of frames to 10 and adds them to the combo box
+        For i As Integer = 1 To 10
+            cmbFrame.Items.Add(i)
+        Next
+
+        'Add the algorithms to the combo box
+        cmbAlgo.Items.Add("FIFO")
+        cmbAlgo.Items.Add("LRU")
+        cmbAlgo.Items.Add("OPTIMAL")
+
+    End Sub
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        lblRefString.Text = lblPageRef.Text
+        lblFrames.Text = cmbFrame.Text
+
+
+        'Check if there is a reference string, if no. of frames selected, and if an algorithm is selected
+        If lblPageRef.Text = "--" Then
+            MessageBox.Show("Please generate a reference string first.", "Input Needed", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        ElseIf cmbFrame.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select the number of frames.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        ElseIf cmbAlgo.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select an Algorithm", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+
+        'Parse the selected number of frames from the combo box into an integer
+        Frames = Integer.Parse(cmbFrame.SelectedItem.ToString())
+
+
+        'Call the appropriate algorithm based on the selected item in the combo box
+        DisplayAlgorithms()
+
+        pageFaults = 0
+
+    End Sub
+
+
+End Class
+
